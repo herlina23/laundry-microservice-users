1e072b9976409893161b53a80ab2082f874ce76a
herlina23 <frois.herlina@gmail.com>
2019-04-22 12:13:04 +0700

    slary model edited

--- models/Salary.js
+++ models/Salary.js
@@ -1,21 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Salary Schema
 const salarySchema = mongoose.Schema({
-    date:{
-        type : Date,
-        default: Date.now()
-    },
-    user:{
-        type : mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    total:{
-        type : Number,
-        required: true
-    }
-   
+  date: {
+    type: Date,
+    default: Date.now()
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  employee: {
+    type: String,
+    ref: "Employee",
+    required: true
+  },
+  total: {
+    type: Number,
+    required: true
+  }
 });
 
-const Salary = module.exports = mongoose.model('Salary', salarySchema);
+const Salary = (module.exports = mongoose.model("Salary", salarySchema));
