b6c55d45dc8f249973e80566700a46ad786040d9
daffaputradamar <daffaputradamar@gmail.com>
2019-05-05 23:08:32 +0700

    change report controller

--- routes/mreports.js
+++ routes/mreports.js
@@ -3,20 +3,9 @@ const verifyToken = require("../middlewares/verifyToken");
 const router = express.Router();
 
 const {
-  showSalary,
-  showDetail,
-  showItemin,
-  showItemout,
-  showOutcome,
-  showTransaction
+  index
 } = require("../controllers/mreport");
 
 router.use(verifyToken);
-router.get("/showSalary/", showSalary);
-router.get("/showDetail/", showDetail);
-router.get("/showItemin/", showItemin);
-router.get("/showItemout/", showItemout);
-router.get("/showOutcome/", showOutcome);
-router.get("/showTransaction/", showTransaction);
-
+router.get("/reports", index);
 module.exports = router;
