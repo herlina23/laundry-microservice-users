786b8d7225322f535f1890e1f31556c9971a8335
herlina23 <frois.herlina@gmail.com>
2019-05-09 14:21:02 +0700

    adding the users' name

--- models/User.js
+++ models/User.js
@@ -1,26 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // User Schema
 const userSchema = mongoose.Schema({
-    
-    username:{
-        type : String,
-        unique: true,
-        required: true
-    },
-    role:{
-        type : String,
-        enum: ["admin", "kasir"],
-        required: true
-    },
-    password:{
-        type : String,
-        required: true
-    },
-    create_date:{
-        type : Date,
-        default: Date.now()
-    }
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    unique: true
+  },
+
+  role: {
+    type: String,
+    enum: ["admin", "kasir"],
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  create_date: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
-const User = module.exports = mongoose.model('User', userSchema);
+const User = (module.exports = mongoose.model("User", userSchema));
