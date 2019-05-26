1b9d36ed9566ae57e1936ccd2e741384ff94ecd3
herlina23 <frois.herlina@gmail.com>
2019-05-27 04:53:40 +0700

    modify mreport_new

--- controllers/mreport_new.js
+++ controllers/mreport_new.js
@@ -6,34 +6,23 @@ const Itemout = require("../models/Itemout");
 const Outcome = require("../models/Outcome");
 const Outcomein = require("../models/Outcomein");
 const Transaction = require("../models/Transaction");
+const axios = require("axios");
 
 module.exports = {
-  index: (req, res) => {
+  pemasukan: (req, res) => {
+    let responseObject = {
+      pemasukan: 0
+    };
     let { m, y } = req.query;
     m = parseInt(m);
     y = parseInt(y);
-    let responseObject = {
-      pemasukan: 0,
 
-      pengeluaran: {
-        salaries: [],
-        items: [],
-        outcomes: []
-      },
-      laba: 0,
-      keluar: 0,
-      keluarItem: 0,
-      keluarSalary: 0,
-      KeluarOutcome: 0,
-      bulan: 0,
-      tahun: 0
-    };
-    // Transaction.find()
     Transaction.aggregate([
       {
-        $match: {
-          month: m,
-          year: y
+        $addFields: {
+          "month(dateIn)": {
+            $month: "$dateIn"
+          }
         }
       },
       {
@@ -57,140 +46,232 @@ module.exports = {
           month: "$_id.month(dateIn)",
           totalPay: "$SUM(grandTotal)"
         }
+      },
+      {
+        $match: {
+          month: m,
+          year: y
+        }
       }
-    ])
-      .then(transactions => {
-        //kita dapet hasil dari query sesuai di link tadi
-        //kita filter yg bulan sekarang
-        responseObject.pemasukan = transactions[0].totalPay;
-
-        return Itemin.aggregate([
-          {
-            $match: {
-              month: m,
-              year: y
-            }
-          },
-          {
-            $group: {
-              _id: {
-                "month(create_date)": {
-                  $month: "$create_date"
-                },
-                "year(create_date)": {
-                  $year: "$create_date"
-                }
-                // item: "$item"
-              },
-              "SUM(price)": {
-                $sum: "$price"
-              }
-            }
-          },
-          {
-            $project: {
-              // item: "$_id.item",
-              year: "$_id.year(create_date)",
-              month: "$_id.month(create_date)",
-              bayar_barang: "$SUM(price)"
-            }
+    ]).then(transactions => {
+      responseObject.pemasukan = transactions[0].totalPay;
+      return res.json(responseObject);
+    });
+  },
+  keluarItem: (req, res) => {
+    let { m, y } = req.query;
+    m = parseInt(m);
+    y = parseInt(y);
+    Itemin.aggregate([
+      {
+        $lookup: {
+          from: "items",
+          localField: "item",
+          foreignField: "_id",
+          as: "item"
+        }
+      },
+      {
+        $unwind: "$item"
+      },
+      {
+        $addFields: {
+          month: {
+            $month: "$create_date"
           }
-        ]);
-      })
-
-      .then(itemins => {
-        responseObject.pengeluaran.items = itemins;
-        return Salary.aggregate([
-          {
-            $match: {
-              month: m,
-              year: y
+        }
+      },
+      {
+        $group: {
+          _id: {
+            "month(create_date)": {
+              $month: "$create_date"
+            },
+            "year(create_date)": {
+              $year: "$create_date"
             }
           },
-          {
-            $group: {
-              _id: {
-                "month(date)": {
-                  $month: "$create_date"
-                },
-                "year(date)": {
-                  $year: "$create_date"
-                }
-              },
-              "SUM(total)": {
-                $sum: "$total"
-              }
-            }
-          },
-          {
-            $project: {
-              year: "$_id.year(date)",
-              month: "$_id.month(date)",
-              paysalary: "$SUM(total)"
-            }
+          "SUM(price)": {
+            $sum: "$price"
           }
-        ]);
-      })
-      .then(salaries => {
-        responseObject.pengeluaran.salaries = salaries;
-        return Outcome.aggregate([
-          {
-            $match: {
-              month: m,
-              year: y
-            }
-          },
-          {
-            $group: {
-              _id: {
-                "month(date)": {
-                  $month: "$date"
-                },
-                "year(date)": {
-                  $year: "$date"
-                }
-              },
-              "SUM(total)": {
-                $sum: "$total"
-              }
-            }
-          },
-          {
-            $project: {
-              year: "$_id.year(date)",
-              month: "$_id.month(date)",
-              paysalary: "$SUM(total)"
-            }
+        }
+      },
+      {
+        $project: {
+          year: "$_id.year(create_date)",
+          month: "$_id.month(create_date)",
+          bayar_barang: "$SUM(price)"
+        }
+      },
+      {
+        $match: {
+          month: m,
+          year: y
+        }
+      }
+    ]).then(itemins => res.json(itemins));
+  },
+  keluarSalary: (req, res) => {
+    let { m, y } = req.query;
+    m = parseInt(m);
+    y = parseInt(y);
+    Salary.aggregate([
+      {
+        $lookup: {
+          from: "users",
+          localField: "user",
+          foreignField: "_id",
+          as: "user"
+        }
+      },
+      {
+        $unwind: "$user"
+      },
+      {
+        $addFields: {
+          month: {
+            $month: "$create_date"
           }
-        ]);
-      })
-      .then(outcomes => {
-        responseObject.pengeluaran.outcomes = outcomes;
+        }
+      },
+      {
+        $group: {
+          _id: {
+            "month(date)": {
+              $month: "$create_date"
+            },
+            "year(date)": {
+              $year: "$create_date"
+            }
+          },
+          "SUM(total)": {
+            $sum: "$total"
+          }
+        }
+      },
+      {
+        $project: {
+          year: "$_id.year(date)",
+          month: "$_id.month(date)",
+          paysalary: "$SUM(total)"
+        }
+      },
+      {
+        $match: {
+          month: m,
+          year: y
+        }
+      }
+    ]).then(salaries => res.json(salaries));
+  },
+  keluarOutcome: (req, res) => {
+    let { m, y } = req.query;
+    m = parseInt(m);
+    y = parseInt(y);
 
-        responseObject.bulan = responseObject.pengeluaran.outcomes[0].month;
-        responseObject.tahun = responseObject.pengeluaran.outcomes[0].year;
+    Outcome.aggregate([
+      {
+        $lookup: {
+          from: "outcomeins",
+          localField: "outcomein",
+          foreignField: "_id",
+          as: "outcomein"
+        }
+      },
+      {
+        $unwind: "$outcomein"
+      },
+      {
+        $addFields: {
+          month: {
+            $month: "$date"
+          }
+        }
+      },
+      {
+        $group: {
+          _id: {
+            "month(date)": {
+              $month: "$date"
+            },
+            "year(date)": {
+              $year: "$date"
+            }
+          },
+          "SUM(total)": {
+            $sum: "$total"
+          }
+        }
+      },
+      {
+        $project: {
+          year: "$_id.year(date)",
+          month: "$_id.month(date)",
+          paybill: "$SUM(total)"
+        }
+      },
+      {
+        $match: {
+          month: m,
+          year: y
+        }
+      }
+    ]).then(outcomes => res.json(outcomes));
+  },
+  keluarOutcome2: (req, res) => {
+    // let dateNow = new Date();
+    let { m, y } = req.query;
+    m = parseInt(m);
+    y = parseInt(y);
 
-        responseObject.keluarItem =
-          responseObject.pengeluaran.items[0].bayar_barang;
-
-        responseObject.KeluarOutcome =
-          responseObject.pengeluaran.outcomes[0].paysalary;
-
-        responseObject.keluarSalary =
-          responseObject.pengeluaran.salaries[0].paysalary;
-
-        responseObject.keluar =
-          responseObject.pengeluaran.items[0].bayar_barang +
-          responseObject.pengeluaran.outcomes[0].paysalary +
-          responseObject.pengeluaran.salaries[0].paysalary;
-
-        // cara menghitung labanya gimana ? (pemasukan - pengeluaran), tapi kenapa hasil laba:null ?
-        responseObject.laba =
-          responseObject.pemasukan -
-          (responseObject.pengeluaran.items[0].bayar_barang +
-            responseObject.pengeluaran.outcomes[0].paysalary +
-            responseObject.pengeluaran.salaries[0].paysalary);
-        return res.json(responseObject);
-      });
+    // console.log(req.query);
+    Outcome.aggregate([
+      {
+        $lookup: {
+          from: "outcomeins",
+          localField: "outcomein",
+          foreignField: "_id",
+          as: "outcomein"
+        }
+      },
+      {
+        $unwind: "$outcomein"
+      },
+      {
+        $addFields: {
+          month: {
+            $month: "$date"
+          }
+        }
+      },
+      {
+        $group: {
+          _id: {
+            "month(date)": {
+              $month: "$date"
+            },
+            "year(date)": {
+              $year: "$date"
+            }
+          },
+          "SUM(total)": {
+            $sum: "$total"
+          }
+        }
+      },
+      {
+        $project: {
+          year: "$_id.year(date)",
+          month: "$_id.month(date)",
+          paybill: "$SUM(total)"
+        }
+      },
+      {
+        $match: {
+          month: m,
+          year: y
+        }
+      }
+    ]).then(outcomes => res.json(outcomes));
   }
 };
--- routes/mreports.js
+++ routes/mreports.js
@@ -2,8 +2,20 @@ const express = require("express");
 const verifyToken = require("../middlewares/verifyToken");
 const router = express.Router();
 
-const { index } = require("../controllers/mreport_new");
-router.get("/report", index);
+const {
+  index,
+  pemasukan,
+  keluarItem,
+  keluarOutcome,
+  keluarOutcome2,
+  keluarSalary
+} = require("../controllers/mreport_new");
+//router.get("/report", index);
+router.get("/pemasukan", pemasukan);
+router.get("/item", keluarItem);
+router.get("/outcome", keluarOutcome);
+router.get("/outcome2", keluarOutcome2);
+router.get("/salary", keluarSalary);
 router.use(verifyToken);
 
 module.exports = router;
